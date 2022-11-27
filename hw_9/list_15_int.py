8ed907a9347efe00913fe37cb03b2d6bdabff213
Yevgen Dreval <master33life@ukr.net.com>
2022-11-27 12:33:03 +0200

    виправив додавання у list_15_int.py, виправив довжини строк

--- hw_9/list_15_int.py
+++ hw_9/list_15_int.py
@@ -7,9 +7,11 @@ an_list = [random.randrange(-1001, 1001) for i in range(15)]
 print("Ваш список має вигляд:", an_list)
 
 # виконую аналіз за умовою
-# вважаю, що список починається з парного числа (нульового)
-print("Чи більша сумма непарних чисел у списку за суму парних чисел? - ", end="")
-if sum(j for i, j in enumerate(an_list) if i % 2 != 0) > sum(j for i, j in enumerate(an_list) if i % 2 == 0):
+print("Чи більша сума непарних чисел у списку",
+      "за суму парних чисел? - ", end="")
+even = sum(i for i in an_list if i % 2 == 0)
+odd = sum(j for j in an_list if j % 2 != 0)
+if even < odd:
     print("Так.")
 else:
     print("Ні.")
